f2c30982ca52e6132a2ce21964df81caed223695
songguoxing <980538137@qq.com>
2013-05-29 17:29:51 +0800

    mainservice

--- 5_18_sina/src/cn/edu/nuc/weibo/logic/MainService.java
+++ 5_18_sina/src/cn/edu/nuc/weibo/logic/MainService.java
@@ -115,78 +115,78 @@ public class MainService extends Service implements Runnable {
 		String return_msg = null;
 		List<Status> mStatuses = null;
 		List<Comment> mComments = null;
-		WeiboHomeService mweiboHomeService = new WeiboHomeService(this);
-		switch (task.getTaskId()) {
-			case Task.WEIBO_USER_INFO :// 获取用户信息
-				return_msg = WeiboUtils.getUserInfo(mWeibo, Weibo.getAppKey());
-				msg.obj = return_msg;
-				break;
-			case Task.WEIBO_STATUSES_FRIENDS_TIMELINE :// 获取当前登录用户所关注用户的最新微博
-				mStatuses = WeiboUtils
-						.getFriendsTimeLine(task, mweiboHomeService);
-				msg.obj = mStatuses;
-				break;
-			case Task.WEIBO_STATUSES_UPDATE :// 发布一条微博
-				return_msg = WeiboUtils.update(mWeibo, Weibo.getAppKey(),
-						(HashMap<String, Object>) task.getTaskParams());
-				break;
-			case Task.WEIBO_STATUSES_UPLOAD :// 发布一条带图片的的微博
-				return_msg = WeiboUtils.upload(mWeibo, Weibo.getAppKey(),
-						(HashMap<String, Object>) task.getTaskParams());
-				break;
-			case Task.WEIBO_STATUSES_COMMENT :// 评论微博
-				return_msg = WeiboUtils.comment(mWeibo, Weibo.getAppKey(),
-						(HashMap<String, Object>) task.getTaskParams());
-				break;
-			case Task.WEIBO_STATUSES_REPOST :// 转发微博
-				return_msg = WeiboUtils.repost(mWeibo, Weibo.getAppKey(),
-						(HashMap<String, Object>) task.getTaskParams());
-				break;
-			case Task.WEIBO_FAVORITE_CREATE :// 收藏微博
-				return_msg = WeiboUtils.favorite(mWeibo, Weibo.getAppKey(),
-						(HashMap<String, Object>) task.getTaskParams(), true);
-				break;
-			case Task.WEIBO_FAVORITE_DESTROY :
-				return_msg = WeiboUtils.favorite(mWeibo, Weibo.getAppKey(),
-						(HashMap<String, Object>) task.getTaskParams(), false);
-				break;
-			case Task.WEIBO_STATUSES_DESTROY :
 
-				break;
-			case Task.WEIBO_MSG_AT_WB :// 获取@当前用户的微博
-				mStatuses = WeiboUtils.getStatusesMentions(mWeibo,
-						Weibo.getAppKey(), mTaskParams);
-				msg.obj = mStatuses;
-				break;
-			case Task.WEIBO_MSG_AT_COMMENT :// 获取@当前用户的评论
-				mComments = WeiboUtils.getCommentsMentions(mWeibo,
-						Weibo.getAppKey(), mTaskParams);
-				msg.obj = mComments;
-				break;
-			case Task.WEIBO_MSG_COMMENT_BY_ME :// 获取当前用户收到的评论和发出的评论
-				mComments = WeiboUtils.getCommentsByMe(mWeibo, Weibo.getAppKey(),
-						(HashMap<String, Object>) task.getTaskParams());
-				msg.obj = mComments;
-				break;
-			case Task.WEIBO_MSG_COMMENT_TO_ME :
-				mComments = WeiboUtils.getCommentsToMe(mWeibo, Weibo.getAppKey(),
-						mTaskParams);
-				msg.obj = mComments;
-				break;
-			case Task.WEIBO_MSG_MESSAGE :// 当前用户的私信
-				break;
-			case Task.WEIBO_MSG_NOTIFICATION :// 当前用户的通知
-				break;
-			case Task.WEIBO_EMOTIONS :
-				return_msg = WeiboUtils.getEmotions();
-				break;
-			case Task.WEIBO_PLACE_USER_TIMELINE :
-				mStatuses = WeiboUtils.getPlaceUserTimeline(mWeibo,
-						Weibo.getAppKey(), mTaskParams);
-				Geos geos = WeiboUtils.getGeoToAddress(mWeibo,
-						Weibo.getAppKey(), mStatuses);
-				msg.obj = geos;
-				break;
+		switch (task.getTaskId()) {
+		case Task.WEIBO_USER_INFO:// 获取用户信息
+			return_msg = WeiboUtils.getUserInfo(mWeibo, Weibo.getAppKey());
+			msg.obj = return_msg;
+			break;
+		case Task.WEIBO_STATUSES_FRIENDS_TIMELINE:// 获取当前登录用户所关注用户的最新微博
+			WeiboHomeService mweiboHomeService = new WeiboHomeService(this);
+			mStatuses = WeiboUtils.getFriendsTimeLine(task, mweiboHomeService);
+			msg.obj = mStatuses;
+			break;
+		case Task.WEIBO_STATUSES_UPDATE:// 发布一条微博
+			return_msg = WeiboUtils.update(mWeibo, Weibo.getAppKey(),
+					(HashMap<String, Object>) task.getTaskParams());
+			break;
+		case Task.WEIBO_STATUSES_UPLOAD:// 发布一条带图片的的微博
+			return_msg = WeiboUtils.upload(mWeibo, Weibo.getAppKey(),
+					(HashMap<String, Object>) task.getTaskParams());
+			break;
+		case Task.WEIBO_STATUSES_COMMENT:// 评论微博
+			return_msg = WeiboUtils.comment(mWeibo, Weibo.getAppKey(),
+					(HashMap<String, Object>) task.getTaskParams());
+			break;
+		case Task.WEIBO_STATUSES_REPOST:// 转发微博
+			return_msg = WeiboUtils.repost(mWeibo, Weibo.getAppKey(),
+					(HashMap<String, Object>) task.getTaskParams());
+			break;
+		case Task.WEIBO_FAVORITE_CREATE:// 收藏微博
+			return_msg = WeiboUtils.favorite(mWeibo, Weibo.getAppKey(),
+					(HashMap<String, Object>) task.getTaskParams(), true);
+			break;
+		case Task.WEIBO_FAVORITE_DESTROY:
+			return_msg = WeiboUtils.favorite(mWeibo, Weibo.getAppKey(),
+					(HashMap<String, Object>) task.getTaskParams(), false);
+			break;
+		case Task.WEIBO_STATUSES_DESTROY:
+
+			break;
+		case Task.WEIBO_MSG_AT_WB:// 获取@当前用户的微博
+			mStatuses = WeiboUtils.getStatusesMentions(mWeibo,
+					Weibo.getAppKey(), mTaskParams);
+			msg.obj = mStatuses;
+			break;
+		case Task.WEIBO_MSG_AT_COMMENT:// 获取@当前用户的评论
+			mComments = WeiboUtils.getCommentsMentions(mWeibo,
+					Weibo.getAppKey(), mTaskParams);
+			msg.obj = mComments;
+			break;
+		case Task.WEIBO_MSG_COMMENT_BY_ME:// 获取当前用户收到的评论和发出的评论
+			mComments = WeiboUtils.getCommentsByMe(mWeibo, Weibo.getAppKey(),
+					(HashMap<String, Object>) task.getTaskParams());
+			msg.obj = mComments;
+			break;
+		case Task.WEIBO_MSG_COMMENT_TO_ME:
+			mComments = WeiboUtils.getCommentsToMe(mWeibo, Weibo.getAppKey(),
+					mTaskParams);
+			msg.obj = mComments;
+			break;
+		case Task.WEIBO_MSG_MESSAGE:// 当前用户的私信
+			break;
+		case Task.WEIBO_MSG_NOTIFICATION:// 当前用户的通知
+			break;
+		case Task.WEIBO_EMOTIONS:
+			return_msg = WeiboUtils.getEmotions();
+			break;
+		case Task.WEIBO_PLACE_USER_TIMELINE:
+			mStatuses = WeiboUtils.getPlaceUserTimeline(mWeibo,
+					Weibo.getAppKey(), mTaskParams);
+			Geos geos = WeiboUtils.getGeoToAddress(mWeibo, Weibo.getAppKey(),
+					mStatuses);
+			msg.obj = geos;
+			break;
 		}
 		handler.sendMessage(msg);
 	}
@@ -196,68 +196,68 @@ public class MainService extends Service implements Runnable {
 
 		public void handleMessage(android.os.Message msg) {
 			switch (msg.what) {
-				case Task.WEIBO_USER_INFO :
-					break;
-				case Task.WEIBO_STATUSES_FRIENDS_TIMELINE :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("HomeActivity");
-					iWeiboActivity.refresh(msg.obj);
-					break;
-				case Task.WEIBO_STATUSES_UPDATE :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("NewWeiboActivity");
-					iWeiboActivity.refresh(msg.obj, "update");
-					break;
-				case Task.WEIBO_STATUSES_UPLOAD :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("NewWeiboActivity");
-					iWeiboActivity.refresh(msg.obj, "upload");
-					break;
-				case Task.WEIBO_STATUSES_COMMENT :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("CommentActivity");
-					iWeiboActivity.refresh(msg.obj);
-					break;
-				case Task.WEIBO_FAVORITE_CREATE :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("DetailWeiboActivity");
-					iWeiboActivity.refresh(msg.obj, "create");
-					break;
-				case Task.WEIBO_FAVORITE_DESTROY :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("DetailWeiboActivity");
-					iWeiboActivity.refresh(msg.obj, "destroy");
-					break;
-				case Task.WEIBO_MSG_AT_WB :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("MsgActivity");
-					iWeiboActivity.refresh(msg.obj, MsgActivity.AT, "status");
-					break;
-				case Task.WEIBO_MSG_AT_COMMENT :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("MsgActivity");
-					iWeiboActivity.refresh(msg.obj, MsgActivity.AT, "comment");
-					break;
-				case Task.WEIBO_MSG_COMMENT_BY_ME :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("MsgActivity");
-					iWeiboActivity.refresh(msg.obj, MsgActivity.COMMENT);
-					break;
-				case Task.WEIBO_MSG_COMMENT_TO_ME :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("MsgActivity");
-					iWeiboActivity.refresh(msg.obj, MsgActivity.COMMENT);
-					break;
-				case Task.WEIBO_PLACE_USER_TIMELINE :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("NewWeiboActivity");
-					iWeiboActivity.refresh(msg.obj, "place");
-					break;
-				case Task.WEIBO_STATUSES_REPOST :
-					iWeiboActivity = (IWeiboActivity) MainService.this
-							.getActivityByName("RedirectActivity");
-					iWeiboActivity.refresh(msg.obj);
-					break;
+			case Task.WEIBO_USER_INFO:
+				break;
+			case Task.WEIBO_STATUSES_FRIENDS_TIMELINE:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("HomeActivity");
+				iWeiboActivity.refresh(msg.obj);
+				break;
+			case Task.WEIBO_STATUSES_UPDATE:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("NewWeiboActivity");
+				iWeiboActivity.refresh(msg.obj, "update");
+				break;
+			case Task.WEIBO_STATUSES_UPLOAD:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("NewWeiboActivity");
+				iWeiboActivity.refresh(msg.obj, "upload");
+				break;
+			case Task.WEIBO_STATUSES_COMMENT:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("CommentActivity");
+				iWeiboActivity.refresh(msg.obj);
+				break;
+			case Task.WEIBO_FAVORITE_CREATE:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("DetailWeiboActivity");
+				iWeiboActivity.refresh(msg.obj, "create");
+				break;
+			case Task.WEIBO_FAVORITE_DESTROY:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("DetailWeiboActivity");
+				iWeiboActivity.refresh(msg.obj, "destroy");
+				break;
+			case Task.WEIBO_MSG_AT_WB:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("MsgActivity");
+				iWeiboActivity.refresh(msg.obj, MsgActivity.AT, "status");
+				break;
+			case Task.WEIBO_MSG_AT_COMMENT:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("MsgActivity");
+				iWeiboActivity.refresh(msg.obj, MsgActivity.AT, "comment");
+				break;
+			case Task.WEIBO_MSG_COMMENT_BY_ME:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("MsgActivity");
+				iWeiboActivity.refresh(msg.obj, MsgActivity.COMMENT);
+				break;
+			case Task.WEIBO_MSG_COMMENT_TO_ME:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("MsgActivity");
+				iWeiboActivity.refresh(msg.obj, MsgActivity.COMMENT);
+				break;
+			case Task.WEIBO_PLACE_USER_TIMELINE:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("NewWeiboActivity");
+				iWeiboActivity.refresh(msg.obj, "place");
+				break;
+			case Task.WEIBO_STATUSES_REPOST:
+				iWeiboActivity = (IWeiboActivity) MainService.this
+						.getActivityByName("RedirectActivity");
+				iWeiboActivity.refresh(msg.obj);
+				break;
 			}
 		}
 	};
